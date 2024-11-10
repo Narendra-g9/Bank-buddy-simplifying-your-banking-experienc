1d204b558e5a8d2fea435690412b2df80aa7643f
Narendra-g9 <218x1a05g9@khitguntur.ac.in>
2024-11-10 17:41:43 +0530

    Add files via upload

---
+++ Backend/BACKEND/src/middlewares/verifyAdmin.js
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyAdminToken } from "./verifyTokens";
+
+export const verifyAdmin = (
+  reqt,
+  res,
+  next
+) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ msg: "Authorization header is missing" });
+  }
+  const token = authHeader.split(" ")[1];
+  const decoded = verifyAdminToken(token);
+  if (!decoded) {
+    return res.status(401).send({ msg: "Access denied. Admins only." });
+  }
+  next();
+};
